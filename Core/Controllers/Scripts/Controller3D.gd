c0a15523ca58bbb5041ec45369a853f0eebf2e4d
Ezma Green <EzmaMeow@hotmail.com>
2025-10-03 16:27:22 -0400

    Delete Core directory
    I mess up

--- Core/Controllers/Scripts/Controller3D.gd
+++
@@ -1,9 +0,0 @@
-class_name Controller3D extends Controller_Base
-
-signal rotated(axis:Vector3,angle:float)
-
-@export var move_direction : Vector3
-@export var look_direction : Vector3 
-
-func rotate(axis:Vector3,angle:float):
-	rotated.emit(axis,angle)
